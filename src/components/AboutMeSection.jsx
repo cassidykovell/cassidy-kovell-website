6744da409e98d7dc15adb0ee2d04f85b4a6dc694
Cassidy <cassidy0kovell@gmail.com>
2024-06-13 10:25:40 -0700

    about me fix

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -5,7 +5,7 @@ const AboutMeSection = () => {
       <section id="section1">
         <h3>About Me</h3>
         <p className="content">
-          Full Stack web developer utilizing a background in English to provide inspired and quality code developed through a technical eye. In the process of earning a Full Stack Web Development certificate from the University of California Los Angeles Coding Boot Camp. Inventive problem solver who works well in a team environment as well as self-reliantly. Extremely passionate and hard-working with strengths in project conception and execution, flexibility, creativity, and logical thinking. Consistent and willing to go above and beyond for the betterment of a project.
+          Full Stack web developer utilizing a background in English to provide inspired and quality code developed through a technical eye. Earned a Full Stack Web Development certificate from the University of California Los Angeles Coding Boot Camp. Inventive problem solver who works well in a team environment as well as self-reliantly. Extremely passionate and hard-working with strengths in project conception and execution, flexibility, creativity, and logical thinking. Consistent and willing to go above and beyond for the betterment of a project.
         </p>
       </section>
     );
