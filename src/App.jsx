94e21738154593b6a099487835532d2df2f1998f
Cassidy <cassidy0kovell@gmail.com>
2024-06-13 10:17:16 -0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,9 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 import './index.css';
 
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  console.log("Token retrieved from localStorage:", token);
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-});
-
 const App = () => {
   return (
-    <ApolloProvider client={client}>
+    <div>
       <div id="header">
       <Header />
       <Nav />
@@ -43,7 +20,7 @@ const App = () => {
         <Outlet />
       </main>
       <Footer />
-    </ApolloProvider>
+    </div>
   );
 };
 
