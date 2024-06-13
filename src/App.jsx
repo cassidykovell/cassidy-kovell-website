1896e92fc5d74be0ee69549041dd6b7feced0bae
Cassidy <cassidy0kovell@gmail.com>
2024-06-13 10:09:54 -0700

    more updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import { Outlet } from 'react-router-dom';
 import './index.css';
 
-const App = () => {
-  const [activeSection, setActiveSection] = useState('aboutMe');
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-  const handleNavClick = (sectionId) => {
-    setActiveSection(sectionId);
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  console.log("Token retrieved from localStorage:", token);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
   };
+});
 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+const App = () => {
   return (
-    <div>
+    <ApolloProvider client={client}>
       <div id="header">
       <Header />
       <Nav />
@@ -26,7 +43,7 @@ const App = () => {
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </ApolloProvider>
   );
 };
 
