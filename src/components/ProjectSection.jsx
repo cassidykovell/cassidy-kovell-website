71e67a5100aaa6ec314ee21491326a595ed37535
Cassidy <cassidy0kovell@gmail.com>
2024-08-13 20:03:25 -0700

    added description to projects

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -14,42 +14,49 @@ const ProjectsSection = () => {
             deployedLink: 'https://flash-me-8481dc8f6f3f.herokuapp.com/',
             githubLink: 'https://github.com/cassidykovell/flash-me',
             imageSrc: project1,
+            description: 'This site functions as a social media-like platform where users can create collections of flashcards and post them to their feeds. They can also view collections posted by other users on their feeds and revisit them under their profiles.',
         },
         {
             title: "With What's In The Fridge",
             deployedLink: 'https://with-whats-in-the-fridge.onrender.com/',
             githubLink: 'https://github.com/cassidykovell/with-whats-in-the-fridge',
             imageSrc: project7,
+            description: 'This web application allows users to enter the ingredients in their fridge into their online "fridge" and have recipes returned that match precisely what they have at home. They can also add recipes to their own online cookbook, post their own recipes, and save other recipes they view on their feed.',
         },
         {
             title: 'Date Night Generator',
             deployedLink: 'https://cassidykovell.github.io/date-night/',
             githubLink: 'https://github.com/cassidykovell/date-night',
             imageSrc: project2,
+            description: 'This site allows users to have a randomly generated cocktail recipe and a randomly generated dinner recipe to provide the user with fun and experimental date nights.',
         },
         {
             title: 'Weather App',
             deployedLink: 'https://cassidykovell.github.io/weather-website/',
             githubLink: 'https://github.com/cassidykovell/weather-website',
             imageSrc: project3,
+            description: 'This application is a weather app. Users can enter their location and bring up the weather for that day and the entire week.',
         },
         {
             title: 'Daily Planner',
             deployedLink: 'https://cassidykovell.github.io/daily-planner/',
             githubLink: 'https://github.com/cassidykovell/daily-planner',
             imageSrc: project4,
+            description: 'A user can add their daily plans to this site, and the data will be saved persistently. The current date and time will also be displayed.',
         },
         {
             title: 'Timed Coding Quiz',
             deployedLink: 'https://cassidykovell.github.io/timed-coding-quiz/',
             githubLink: 'https://github.com/cassidykovell/timed-coding-quiz',
             imageSrc: project5,
+            description: 'A quiz to test users on their programming knowledge and skills.',
         },
         {
             title: 'CSS Cheat Sheet',
             deployedLink: 'https://cassidykovell.github.io/css-cheatsheet/',
             githubLink: 'https://github.com/cassidykovell/css-cheatsheet',
             imageSrc: project6,
+            description: 'A site that displayed useful CSS code for developers.',
         },
     ];
 
@@ -67,6 +74,10 @@ const ProjectsSection = () => {
         <section id="section2">
             <h3>Projects</h3>
             <div className="content">
+            <div>
+                <h1>{projects[currentProjectIndex].title}</h1>
+                <p>{projects[currentProjectIndex].description}</p>
+            </div>
                 <div className="project-slideshow">
                     <div className="project-item">
                         <a href={projects[currentProjectIndex].deployedLink} target="_blank">
@@ -74,7 +85,7 @@ const ProjectsSection = () => {
                                 <img className="img" src={projects[currentProjectIndex].imageSrc} alt={projects[currentProjectIndex].title} />
                                 <div className="image-overlay">
                                     <p className="project-title">
-                                        <a id="ptitle" href={projects[currentProjectIndex].deployedLink} target="_blank">{projects[currentProjectIndex].title} </a>
+                                        <a id="ptitle" href={projects[currentProjectIndex].deployedLink} target="_blank">Click to View!</a>
                                     </p>
                                     <a id="ibackground" href={projects[currentProjectIndex].githubLink} target="_blank" className="github-icon">
                                         <i className="fab fa-github" style={{ fontSize: "1.5rem" }}></i>
